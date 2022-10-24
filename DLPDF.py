47c6bc66217daba48e1aab377727482d0f2a7d92
Sudo Jvck <76821580+SudoJvck@users.noreply.github.com>
2022-10-24 14:57:01 -0400

    Create DLPDF.py

---
+++ DLPDF.py
@@ -0,0 +1,7 @@
+#Download & Save PDF Files
+
+import urllib.request
+url=input("Enter Link to Download PDF : ")
+Name=input("Enter a Name for the PDF File : ")
+FileName = Name+".pdf"
+urllib.request.urlretrieve(url,FileName)
